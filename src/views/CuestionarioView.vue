3bc2b5cdd14c1ec28572207297aa49f8ea4e81a6
rubenphi <rubenphii@gmail.com>
2021-12-28 23:58:07 +0000

    se mejoraron varios aspectos de pregunta

--- src/views/CuestionarioView.vue
+++ src/views/CuestionarioView.vue
@@ -28,23 +28,23 @@
       </ion-card>
       <ion-card>
         <ion-list>
-          <ion-item>
-            <ion-icon slot="start" href="/pregunta" :icon="handLeftOutline"></ion-icon>
+          <ion-item href="/pregunta">
+            <ion-icon slot="start"  :icon="handLeftOutline"></ion-icon>
             <ion-label
               >¿Cuál de los siguientes documentos corresponde al a la
               información entregada en el siguiente enunciado?</ion-label
             >
           </ion-item>
-          <ion-item>
-            <ion-icon slot="start" href="/pregunta" :icon="handLeftOutline"></ion-icon>
+          <ion-item href="/pregunta">
+            <ion-icon slot="start" :icon="handLeftOutline"></ion-icon>
             <ion-label
               >Los presentes textos corresponden a un tipo de texto, analice las
               diferentes características del texto e identifique a qué tipo de
               text corresponde</ion-label
             >
           </ion-item>
-          <ion-item>
-            <ion-icon slot="start" href="/pregunta" :icon="handLeftOutline"></ion-icon>
+          <ion-item href="/pregunta">
+            <ion-icon slot="start" :icon="handLeftOutline"></ion-icon>
             <ion-label
               >¿Cuál de los siguientes documentos corresponde al a la
               información entregada en el siguiente enunciado?</ion-label
--- src/views/PreguntaView.vue
+++ src/views/PreguntaView.vue
@@ -3,13 +3,13 @@
     <ion-header>
       <ion-toolbar>
         <ion-buttons slot="start" class="ion-margin-start">
-          <ion-button href="/cuestionarios">
+          <ion-button href="/cuestionario">
             <ion-icon :icon="arrowBackOutline"></ion-icon>
           </ion-button>
         </ion-buttons>
         <ion-buttons slot="end" class="ion-margin-end">
-          <ion-button href="/cuestionario">
-            <ion-icon :icon="refreshOutline"></ion-icon>
+          <ion-button href="/">
+            <ion-icon :icon="eyeOutline"></ion-icon>
           </ion-button>
         </ion-buttons>
       </ion-toolbar>
@@ -23,11 +23,22 @@
         </ion-card-header>
       </ion-card>
       <ion-card>
-        <ion-img src="https://s1.significados.com/foto/897px-eugene-delacroix-le-28-juillet.-la-liberte-guidant-le-peuple_bg.jpg"></ion-img>
+        <ion-img
+          src="https://s1.significados.com/foto/897px-eugene-delacroix-le-28-juillet.-la-liberte-guidant-le-peuple_bg.jpg"
+        ></ion-img>
       </ion-card>
-      <ion-card-content>
-      El Romanticismo es un movimiento cultural que se originó en Alemania y en Reino Unido a finales del siglo XVIII como una reacción revolucionaria contra la Ilustración y el Neoclasicismo, confiriendo prioridad a los sentimientos. Es considerado como el primer movimiento de cultura que cubrió el mapa completo de Europa. En la mayoría de las áreas estuvo en su apogeo en el período aproximado de 1800 a 1850. Luego, cedió su lugar al positivismo, que fomenta el pensamiento crítico y el empirismo como bases del conocimiento y la sociedad.
-    </ion-card-content>
+      <ion-card>
+        <ion-card-content>
+          El Romanticismo es un movimiento cultural que se originó en Alemania y
+          en Reino Unido a finales del siglo XVIII como una reacción
+          revolucionaria contra la Ilustración y el Neoclasicismo, confiriendo
+          prioridad a los sentimientos. Es considerado como el primer movimiento
+          de cultura que cubrió el mapa completo de Europa. En la mayoría de las
+          áreas estuvo en su apogeo en el período aproximado de 1800 a 1850.
+          Luego, cedió su lugar al positivismo, que fomenta el pensamiento
+          crítico y el empirismo como bases del conocimiento y la sociedad.
+        </ion-card-content>
+      </ion-card>
     </ion-content>
   </ion-page>
 </template>
@@ -37,6 +48,7 @@ import {
   arrowBackOutline,
   refreshOutline,
   handLeftOutline,
+  eyeOutline,
 } from "ionicons/icons";
 
 import {
@@ -50,7 +62,7 @@ import {
   IonCard,
   IonCardTitle,
   IonCardContent,
-  IonImg
+  IonImg,
 } from "@ionic/vue";
 
 export default {
@@ -65,13 +77,14 @@ export default {
     IonCard,
     IonCardTitle,
     IonCardContent,
-    IonImg
+    IonImg,
   },
   setup() {
     return {
       arrowBackOutline,
       handLeftOutline,
       refreshOutline,
+      eyeOutline,
     };
   },
 };
