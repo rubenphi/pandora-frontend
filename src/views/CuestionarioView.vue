2674b8c6a0a9bf2a08a3b2daebb2ae1239b9ea4c
rubenphi <rubenphii@gmail.com>
2021-12-28 04:33:57 +0000

    se quitta content para ampliar tamaño de lista

--- src/views/CuestionarioView.vue
+++ src/views/CuestionarioView.vue
@@ -24,25 +24,23 @@
         </ion-card-header>
       </ion-card>
       <ion-card>
-        <ion-card-content>
-          <ion-list>
-            <ion-item>
-              <ion-icon slot="start" :icon="handLeftOutline"></ion-icon>
-              <ion-label
-                >¿Cuál de los siguientes documentos corresponde al a la
-                información entregada en el siguiente enunciado?</ion-label
-              >
-            </ion-item>
-            <ion-item>
-              <ion-icon slot="start" :icon="handLeftOutline"></ion-icon>
-              <ion-label
-                >Los presentes textos corresponden a un tipo de texto, analice
-                las diferentes características del texto e identifique a qué
-                tipo de text corresponde</ion-label
-              >
-            </ion-item>
-          </ion-list>
-        </ion-card-content>
+        <ion-list>
+          <ion-item>
+            <ion-icon slot="start" :icon="handLeftOutline"></ion-icon>
+            <ion-label
+              >¿Cuál de los siguientes documentos corresponde al a la
+              información entregada en el siguiente enunciado?</ion-label
+            >
+          </ion-item>
+          <ion-item>
+            <ion-icon slot="start" :icon="handLeftOutline"></ion-icon>
+            <ion-label
+              >Los presentes textos corresponden a un tipo de texto, analice las
+              diferentes características del texto e identifique a qué tipo de
+              text corresponde</ion-label
+            >
+          </ion-item>
+        </ion-list>
       </ion-card>
     </ion-content>
   </ion-page>
