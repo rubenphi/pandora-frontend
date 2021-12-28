4adf4c6bf86131e7b2cd92f57ee487020cee8074
rubenphi <rubenphii@gmail.com>
2021-12-28 23:05:04 +0000

    corredidas dependencias

--- src/views/PreguntaView.vue
+++ src/views/PreguntaView.vue
@@ -25,7 +25,7 @@
       <ion-card>
         <ion-img src="https://s1.significados.com/foto/897px-eugene-delacroix-le-28-juillet.-la-liberte-guidant-le-peuple_bg.jpg"></ion-img>
       </ion-card>
-      El Romanticismo es un movimiento cultural que se originó en Alemania y en Reino Unido a finales del siglo XVIII como una reacción revolucionaria contra la Ilustración y el Neoclasicismo, confiriendo prioridad a los sentimientos.[1]​ Es considerado como el primer movimiento de cultura que cubrió el mapa completo de Europa. En la mayoría de las áreas estuvo en su apogeo en el período aproximado de 1800 a 1850.​ Luego, cedió su lugar al positivismo, que fomenta el pensamiento crítico y el empirismo como bases del conocimiento y la sociedad.
+      El Romanticismo es un movimiento cultural que se originó en Alemania y en Reino Unido a finales del siglo XVIII como una reacción revolucionaria contra la Ilustración y el Neoclasicismo, confiriendo prioridad a los sentimientos. Es considerado como el primer movimiento de cultura que cubrió el mapa completo de Europa. En la mayoría de las áreas estuvo en su apogeo en el período aproximado de 1800 a 1850. Luego, cedió su lugar al positivismo, que fomenta el pensamiento crítico y el empirismo como bases del conocimiento y la sociedad.
     </ion-content>
   </ion-page>
 </template>
@@ -54,12 +54,8 @@ export default {
   components: {
     IonHeader,
     IonToolbar,
-    IonTitle,
     IonContent,
     IonPage,
-    IonList,
-    IonItem,
-    IonLabel,
     IonIcon,
     IonButtons,
     IonButton,
