eb003dacf549e52e5b52b2cda91318b9dc7022c0
rubenphi <rubenphii@gmail.com>
2021-12-31 00:17:36 +0000

    respuestas usamdo if

--- src/views/PreguntaResult.vue
+++ src/views/PreguntaResult.vue
@@ -19,20 +19,15 @@
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-list>
-        <ion-item lines="full" class="ion-padding-end">
-          <ion-icon :icon="happyOutline" size="large" slot="start"></ion-icon>
-          <ion-label color="medium">Resultado Grupo 1</ion-label>
+        <ion-item v-for="respuesta in respuestas" :key="index" lines="full" class="ion-padding-end">
+          <ion-icon v-if="respuesta.puntaje > 0" :icon="happyOutline" size="large" slot="start"></ion-icon>
+          <ion-icon v-else :icon="sadOutline" size="large" slot="start"></ion-icon>
+          
+          <ion-label color="medium">Resultado {{respuesta.grupo}}</ion-label>
           <ion-note slot="end">
-            <ion-text color="success"><h6>Obtienen: +9</h6></ion-text>
-            <ion-text color="warning"><h6>Respuesta: B</h6></ion-text>
-          </ion-note>
-        </ion-item>
-        <ion-item lines="full" class="ion-padding-end">
-          <ion-icon :icon="sadOutline" size="large" slot="start"></ion-icon>
-          <ion-label color="medium">Resultado Grupo 2</ion-label>
-          <ion-note slot="end">
-            <ion-text color="danger"><h6>Obtienen: 0</h6></ion-text>
-            <ion-text color="warning"><h6>Respuesta: B</h6></ion-text>
+            <ion-text v-if="respuesta.puntaje > 0" color="success"><h6>Obtienen: +{{respuesta.puntaje}}</h6></ion-text>
+            <ion-text v-else color="danger"><h6>Obtienen: +{{respuesta.puntaje}}</h6></ion-text>
+            <ion-text color="warning"><h6>Respuesta: {{respuesta.key}}</h6></ion-text>
           </ion-note>
         </ion-item>
       </ion-list>
@@ -49,7 +44,7 @@ import {
   happyOutline,
   sadOutline
 } from "ionicons/icons";
-
+import { ref } from "vue";
 import {
   IonIcon,
   IonPage,
@@ -81,7 +76,16 @@ export default {
     IonLabel,
   },
   setup() {
+
+    const respuestas = ref([
+      {id: 1, grupo:'Grupo 2', puntaje:9, opcion:"B"},
+      {id: 2, grupo:'Grupon 3', puntaje:6, opcion:"B"},
+      {id: 3, grupo:'Grupo 1', puntaje:0, opcion:"C"}
+     
+])
+
     return {
+      respuestas,
       arrowBackOutline,
       handLeftOutline,
       paperPlaneOutline,
