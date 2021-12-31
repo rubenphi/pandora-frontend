289b0d970bacfb1568571caf6841979b5621ed17
rubenphi <rubenphii@gmail.com>
2021-12-31 00:23:27 +0000

    se corrige index

--- src/views/PreguntaResult.vue
+++ src/views/PreguntaResult.vue
@@ -19,7 +19,7 @@
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-list>
-        <ion-item v-for="respuesta in respuestas" :key="index" lines="full" class="ion-padding-end">
+        <ion-item v-for="(respuesta, index) in respuestas"  lines="full" class="ion-padding-end">
           <ion-icon v-if="respuesta.puntaje > 0" :icon="happyOutline" size="large" slot="start"></ion-icon>
           <ion-icon v-else :icon="sadOutline" size="large" slot="start"></ion-icon>
           
@@ -27,7 +27,7 @@
           <ion-note slot="end">
             <ion-text v-if="respuesta.puntaje > 0" color="success"><h6>Obtienen: +{{respuesta.puntaje}}</h6></ion-text>
             <ion-text v-else color="danger"><h6>Obtienen: +{{respuesta.puntaje}}</h6></ion-text>
-            <ion-text color="warning"><h6>Respuesta: {{respuesta.key}}</h6></ion-text>
+            <ion-text color="warning"><h6>Respuesta: {{index}}</h6></ion-text>
           </ion-note>
         </ion-item>
       </ion-list>
