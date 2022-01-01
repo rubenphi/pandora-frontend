b12cc37b213285b4978b97107376eae630f2ea80
rubenphi <rubenphii@gmail.com>
2022-01-01 00:26:26 +0000

    cambio segun tamaño

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -9,8 +9,8 @@
       <ion-grid>
         <ion-row class="ion-align-items-start"> <ion-col>&nbsp;</ion-col></ion-row>
         <ion-row class="ion-align-items-center">
-          <ion-col size="1"> </ion-col>
-          <ion-col size="10" class="ion-align-items-center">
+          <ion-col size-s="1" size-xs="1" size-md="2" size-lg="2" size-xl="4"> </ion-col>
+          <ion-col size-s="10" size-xs="10" size-md="8" size-lg="8" size-xl="6" class="ion-align-items-center">
             <ion-card class="ion-padding">
               <ion-grid>
                 <ion-row>
@@ -85,7 +85,7 @@
                 </ion-button>
               </ion-buttons>
           </ion-col>
-          <ion-col size="2"> </ion-col>
+          <ion-col size-s="1" size-xs="1" size-md="2" size-lg="2" size-xl="4"> </ion-col>
         </ion-row>
         <ion-row class="ion-align-items-end">
           <ion-col>&nbsp;</ion-col>
