e6e2f4737817a73214d4205e0540050409379bc6
rubenphi <rubenphii@gmail.com>
2022-01-01 18:13:51 +0000

    eotra ccorreccion

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -147,7 +147,7 @@ setup() {
       await axios.post('/users/login', login.value)
       .then(response => {
         console.log( response.data.token);
-        if (response.data.token == undefined || response.data.token == 'Error: Request failed with status code 422' ) {
+        if (response.data.token == undefined || response.data.token == null ) {
           error.value.estatus = 1;
           error.value.data = "Error al iniciar sesión"
         }
