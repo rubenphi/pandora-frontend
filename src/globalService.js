b1c7a7fc46a7aaf2666926d85abf318fb23bf2ed
rubenphi <rubenphii@gmail.com>
2022-01-02 15:08:50 -0500

    se intenta actualizar todo

--- src/globalService.js
+++ src/globalService.js
@@ -5,6 +5,6 @@ export function tokenHeader() {
     axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
 }
 
-export let usuario = axios.get("/user/loged").then((response) => {
+export async let usuario = await axios.get("/user/loged").then((response) => {
     return response.data;
   }); 
