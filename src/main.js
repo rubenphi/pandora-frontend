8349f320eeadb76b87072199463df0914ade467f
rubenphi <rubenphii@gmail.com>
2022-01-02 01:52:40 +0000

    intento local storage

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
 import axios from 'axios';
-localStorage.setItem('token', 'Bearer ');
+
 axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
 axios.defaults.baseURL = "https://pandora-quiz.herokuapp.com/public/api/";
 
