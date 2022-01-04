5b76e5827e495b2361905c6707502feed3795a34
rubenphi <rubenphii@gmail.com>
2022-01-03 21:16:43 -0800

    se mueve return

--- src/globalService.js
+++ src/globalService.js
@@ -13,8 +13,9 @@ export async function usuarioGet() {
     axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
     await axios.get("/user/loged").then((response) => {
         localStorage.setItem("usuario", JSON.stringify(response.data));
+        return JSON.parse(localStorage.getItem('usuario'));
       });
-      return JSON.parse(localStorage.getItem('usuario'));
+      
 }
 
 export function datoprueba() {
