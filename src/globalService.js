b7ecd0dd7e448f1b34147af41f7458f3f180de5d
rubenphi <rubenphii@gmail.com>
2022-01-03 20:32:53 -0800

    intento con async await

--- src/globalService.js
+++ src/globalService.js
@@ -9,10 +9,10 @@ export function usuarioGet() {
     return JSON.parse(localStorage.getItem('usuario'));
 }
 */
-export function usuarioGet() {
+export async function usuarioGet() {
     axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
     console.log(localStorage.getItem('token'));
-    axios.get("/user/loged").then((response) => {
+    await axios.get("/user/loged").then((response) => {
         localStorage.setItem("usuario", JSON.stringify(response.data));
       });
       console.log(localStorage.getItem('usuario'));
