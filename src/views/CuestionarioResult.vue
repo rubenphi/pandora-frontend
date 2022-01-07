0b84309a70b2c40c6983166a3e719fc29eecbfe0
rubenphi <rubenphii@gmail.com>
2022-01-06 18:38:48 -0800

    Primer intento cuestionario consulta

--- src/views/CuestionarioResult.vue
+++ src/views/CuestionarioResult.vue
@@ -4,12 +4,12 @@
       <ion-toolbar>
         <ion-title size="large" class="ion-text-center">20-03-2011</ion-title>
         <ion-buttons slot="start" class="ion-margin-start">
-          <ion-button href="/cuestionario">
+          <ion-button :href="'/cuestionario/' + id">
             <ion-icon :icon="arrowBackOutline"></ion-icon>
           </ion-button>
         </ion-buttons>
         <ion-buttons slot="end" class="ion-margin-end">
-          <ion-button href="/ganadores">
+          <ion-button :href="'/ganadores/' + id">
             <ion-icon :icon="refreshOutline"></ion-icon>
           </ion-button>
         </ion-buttons>
@@ -17,17 +17,17 @@
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-list>
-        <ion-item v-for="(respuesta, index) in respuestas" :key="respuesta.id" lines="full" class="ion-padding-end">
+        <ion-item v-for="(respuesta, index) in respuestas" :key="index" lines="full" class="ion-padding-end">
           <ion-icon v-if="index === 0" :icon="trophyOutline" size="large" slot="start"></ion-icon>
-          <ion-icon v-else-if="respuesta.puntaje > 0" :icon="happyOutline" size="large" slot="start"></ion-icon>
+          <ion-icon v-else-if="respuesta.total > 0" :icon="happyOutline" size="large" slot="start"></ion-icon>
           <ion-icon v-else :icon="sadOutline" size="large" slot="start"></ion-icon>
           
           <ion-label color="medium">
-            {{respuesta.grupo}}</ion-label>
+            {{respuesta.grupo.nombre}}</ion-label>
           <ion-note slot="end">
             <ion-text v-if="index === 0" color="warning"><h6><ion-icon :icon="starOutline"></ion-icon>GANADOR<ion-icon :icon="starOutline"></ion-icon></h6></ion-text>
-            <ion-text v-if="respuesta.puntaje > 0" color="success"><h6>Total Puntos: {{respuesta.puntaje}}</h6></ion-text>
-            <ion-text v-else color="danger"><h6>Total Puntos: {{respuesta.puntaje}}</h6></ion-text>
+            <ion-text v-if="respuesta.total > 0" color="success"><h6>Total Puntos: {{respuesta.total}}</h6></ion-text>
+            <ion-text v-else color="danger"><h6>Total Puntos: {{respuesta.total}}</h6></ion-text>
           </ion-note>
         </ion-item>
       </ion-list>
@@ -36,6 +36,13 @@
 </template>
 
 <script>
+
+import axios from "axios";
+import { ref } from "vue";
+import { tokenHeader } from "../globalService";
+import { useRoute } from 'vue-router';
+
+
 import {
   arrowBackOutline,
   refreshOutline,
@@ -47,7 +54,7 @@ import {
   starOutline,
   trophyOutline
 } from "ionicons/icons";
-import { ref } from "vue";
+
 import {
   IonIcon,
   IonPage,
@@ -65,6 +72,7 @@ import {
 
 export default {
   components: {
+    onIonViewDidEnter,
     IonHeader,
     IonToolbar,
     IonContent,
@@ -79,13 +87,22 @@ export default {
     IonLabel,
   },
   setup() {
+    const mroute = useRoute();
+    const { id } = mroute.params;
     const respuestas = ref([
-      { id: 1, grupo: "Grupo 2", puntaje: 9, opcion: "B" },
-      { id: 2, grupo: "Grupo 3", puntaje: 6, opcion: "B" },
-      { id: 3, grupo: "Grupo 1", puntaje: 0, opcion: "C" },
+      {  grupo: {nombre: 'Cargando'}, total: 'Cargando' }
     ]);
+    
+    onIonViewDidEnter(() => {
+       tokenHeader();
+        axios.get("/respuestas/resultado/" + id).then((response) => {
+        respuestas.value = response.data;
+      })
+      
+    });
 
     return {
+      id,
       respuestas,
       arrowBackOutline,
       handLeftOutline,
