0b7c33408c15176c487815006bc692dca62a2a30
rubenphi <rubenphii@gmail.com>
2022-01-11 16:25:06 -0500

    corregido ion icon

--- src/views/CuestionariosPage.vue
+++ src/views/CuestionariosPage.vue
@@ -4,24 +4,25 @@
       <ion-toolbar>
         <ion-title>Cuestionarios</ion-title>
 
-                <ion-buttons slot="end" class="ion-margin-end">
+        <ion-buttons slot="end" class="ion-margin-end">
           <ion-button :href="'/home'">
             <ion-icon :icon="addOutline"></ion-icon>
           </ion-button>
         </ion-buttons>
-
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-
-
-      <ion-card v-for="cuestionario in cuestionarios" :key="cuestionario.id" :href="'/cuestionario/' + cuestionario.id" >
+      <ion-card
+        v-for="cuestionario in cuestionarios"
+        :key="cuestionario.id"
+        :href="'/cuestionario/' + cuestionario.id"
+      >
         <ion-card-header>
-          <ion-card-subtitle>{{cuestionario.curso.nombre}}</ion-card-subtitle>
-          <ion-card-title>{{cuestionario.fecha}}</ion-card-title>
+          <ion-card-subtitle>{{ cuestionario.curso.nombre }}</ion-card-subtitle>
+          <ion-card-title>{{ cuestionario.fecha }}</ion-card-title>
         </ion-card-header>
 
-        <ion-card-content> {{cuestionario.tema}} </ion-card-content>
+        <ion-card-content> {{ cuestionario.tema }} </ion-card-content>
       </ion-card>
     </ion-content>
   </ion-page>
@@ -30,8 +31,7 @@
 <script>
 import axios from "axios";
 import { ref } from "vue";
-import { tokenHeader , usuarioGet } from "../globalService";
-
+import { tokenHeader, usuarioGet } from "../globalService";
 
 import { addOutline } from "ionicons/icons";
 
@@ -47,6 +47,9 @@ import {
   IonToolbar,
   IonTitle,
   IonContent,
+  IonIcon,
+  IonButtons,
+  IonButton,
 } from "@ionic/vue";
 
 export default {
@@ -61,25 +64,25 @@ export default {
     IonTitle,
     IonContent,
     IonPage,
-    addOutline
+    IonIcon,
+    IonButtons,
+    IonButton,
   },
   setup() {
-  let usuario = usuarioGet();
-  const cuestionarios = ref ([]);
-  onIonViewWillEnter(() => {
-        tokenHeader();
-        axios.get("/cuestionarios/" ).then((response) => {
+    let usuario = usuarioGet();
+    const cuestionarios = ref([]);
+    onIonViewWillEnter(() => {
+      tokenHeader();
+      axios.get("/cuestionarios/").then((response) => {
         cuestionarios.value = response.data;
-      })
+      });
     });
 
     return {
       addOutline,
       usuario,
-      cuestionarios
-      
-    }
-       
+      cuestionarios,
+    };
   },
 };
 </script>
