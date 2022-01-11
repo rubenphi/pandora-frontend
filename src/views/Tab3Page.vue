9ea86ff382ed6f59ddebb7f7d16f2edba4da2b1b
rubenphi <rubenphii@gmail.com>
2022-01-11 17:20:29 -0500

    IonIcon no ionicon

--- src/views/Tab3Page.vue
+++ src/views/Tab3Page.vue
@@ -49,7 +49,7 @@ import {
   IonContent,
   IonButton,
   IonButtons,
-  Ionicon
+  IonIcon
 } from "@ionic/vue";
 
 export default {
@@ -66,7 +66,7 @@ export default {
     IonPage,
     IonButtons,
     IonButton,
-    Ionicon
+    IonIcon
   },
   setup() {
     const mroute = useRoute();
