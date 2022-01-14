9e00b0abc6ea24c95d79441045e82083309f48dc
rubenphi <rubenphii@gmail.com>
2022-01-13 21:39:32 -0500

    Quill

--- src/views/CrearPregunta.vue
+++ src/views/CrearPregunta.vue
@@ -1,6 +1,5 @@
 <template>
-a  <QuillEditor v-model:content="inputValue" content-type="html" theme="snow" />
-b <QuillEditor v-model:content="inputValue" content-type="html" class="snow" />
+   <QuillEditor v-model:content="inputValue" content-type="html" />
 </template>
 
 <script>
@@ -20,8 +19,7 @@ b <QuillEditor v-model:content="inputValue" content-type="html" class="snow" />
     setup() {
       const inputValue = ref('<h1>This is header</h1><p>This is paragraph</p>')
       return {
-        inputValue,
-        snow
+        inputValue
       }
     },
   })
