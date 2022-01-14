0a815d974aebe3c3483b29054f30b101a0f2aa26
rubenphi <rubenphii@gmail.com>
2022-01-13 21:54:21 -0500

    Quill vainilla

--- src/views/CrearPregunta.vue
+++ src/views/CrearPregunta.vue
@@ -1,24 +1,27 @@
 <template>
-<div id="editor">
-  <p>Hello World!</p>
-  <p>Some initial <strong>bold</strong> text</p>
-  <p><br></p>
-</div>
+  <div id="editor">
+    <p>
+      Hello World!
+    </p>
+    <p>
+      Some initial <strong>bold</strong> text
+    </p>
+    <p>
+      <br>
+    </p>
+  </div>
 
 </template>
 
 <script>
-  
+
   import '../theme/snow.css';
   import '../quill.js'
-  export default defineComponent({
-    },
+  export default defineComponent({},
     setup() {
       var quill = new Quill('#editor', {
-    theme: 'snow'
-  });
-      return {
-      }
-    },
+        theme: 'snow'
+      });
+      return {}
   })
-  </script>
+</script>
